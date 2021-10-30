e00a1ff8d05561b56cde10b40e4fa20533510337
Joe Hayden <joe@joehayden.net>
2021-10-29 23:37:30 -0500

    Initial repo

--- paths/script/path.py
+++ paths/script/path.py
@@ -1,11 +1,9 @@
 # same folder
 from api_key import apikey
 
-
-
 # different folder
 import sys
-sys.path.append(r'C:\Users\harry\OneDrive\Scripts\examples\paths')
+sys.path.append(r'C:\Users\harry\OneDrive\Scripts\examples')
 from paths.secret.api_key2 import apikey2
 
 print (apikey)
